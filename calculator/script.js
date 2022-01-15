1125c2d045ea2566389aca97a13ba251ffe7f380
Artur Kharisov <93016855+JustArtur@users.noreply.github.com>
2022-01-15 07:02:16 +0300

    Add files via upload

--- calculator/script.js
+++ calculator/script.js
@@ -11,14 +11,31 @@ function backSpace() {
     document.form.textview.value = str.substring(0, str.length-1)
 }
 
+function percent(){
+    string = document.form.textview.value 
+    const massiv = ["+","-","*","/"]
+    for (let i of massiv) {
+        if (string.indexOf(i) != -1) {
+            let index = string.indexOf(i)
+            a = parseFloat(string.substring(index+1))
+            document.from.textview.value = a * 0.01
+        }
+        else {
+            string = parseFloat(string)
+            document.form.textview.value = string * 0.01
+        }
+
+    }
+}
+
 function equal() {
     string = document.form.textview.value 
     const massiv = ["+","-","*","/"]
     for (let i of massiv) {
         if (string.indexOf(i) != -1) {
             let index = string.indexOf(i)
-            a = parseInt(string.substring(0,index))
-            b = parseInt(string.substring(index+1))
+            a = parseFloat(string.substring(0,index))
+            b = parseFloat(string.substring(index+1))
             switch(i) {
                 case '+':
                     document.form.textview.value = a + b;
@@ -32,8 +49,7 @@ function equal() {
                 case '/':
                     document.form.textview.value = a / b
             }
-        }
-        
+        }    
     }
     
 
